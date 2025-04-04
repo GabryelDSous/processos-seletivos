813bcbe02901464b6cbb9b7f69673149107152ac
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2025-04-04 07:30:32 -0300

    Fazendo ligações para cada candidato selecionado

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,38 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		//selecaoCandidatos();
-		imprimirSelecionado();
+		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+		for(String candidato : candidatos) {
+			entrandoEmContato( candidato);
+		}
+	}
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizada = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando = !atendeu) {
+				tentativasRealizada++;
+			} else {
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			}
+		} while(continuarTentando && tentativasRealizada < 3);
+		
+		if(atendeu) {
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizada + " TENTATIVAS");
+		} else {
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NÚMERO MAXIMO DE TENTATIVAS " + tentativasRealizada);
+		}
+	}
+	
+	// método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 	
 	static void imprimirSelecionado() {
